5a2aa5225dce0ed3590d7dcecfc6472a08c18953
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2025-01-19 19:42:36 -0600

    Update build-ffmpeg.sh
    Signed-off-by: xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -1585,8 +1585,35 @@ if build "$repo_name" "${version//\$ /}"; then
     echo "Cloning \"$repo_name\" saving version \"$version\""
     git_clone "$git_url" "libtiff-git"
     mkdir -p config
-    chmod +x autogen.sh
-    execute ./autogen.sh
+    retval=0
+
+    set -x
+
+    case `uname` in
+      Darwin*)
+        glibtoolize --force --copy || retval=$?
+        ;;
+      *)
+        libtoolize --force --copy || retval=$?
+        ;;
+    esac
+    aclocal -I ./m4 || retval=$?
+    autoheader || retval=$?
+    automake --foreign --add-missing --copy || retval=$?
+    autoconf || retval=$?
+    # Get latest config.guess and config.sub from upstream master since
+    # these are often out of date.
+    for file in config.guess config.sub
+    do
+        echo "$0: getting $file..."
+        wget -q --timeout=5 -O config/$file.tmp \
+          "https://git.savannah.gnu.org/cgit/config.git/plain/${file}" \
+          && mv -f config/$file.tmp config/$file \
+          && chmod a+x config/$file || retval=$?
+        rm -f config/$file.tmp
+    done
+
+    exit $retval
     execute ./configure --prefix="$workspace" --disable-{docs,sphinx,tests} --enable-cxx --with-pic
     execute make "-j$threads"
     execute sudo make install
