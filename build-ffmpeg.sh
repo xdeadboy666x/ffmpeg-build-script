87d89acb5feffccb0ba2952b2856ba34a0de74e3
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2025-01-20 11:54:13 -0600

    Update build-ffmpeg.sh
    Signed-off-by: xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -1804,12 +1804,11 @@ if build "libjpeg-turbo" "$repo_version"; then
 fi
 
 if "$NONFREE_AND_GPL"; then
-    git_caller "https://github.com/breakfastquay/rubberband.git" "rubberband-git"
+    git_caller "https://github.com/m-ab-s/rubberband.git" "rubberband-git"
     if build "$repo_name" "${version//\$ /}"; then
         echo "Cloning \"$repo_name\" saving version \"$version\""
         git_clone "$git_url"
-	execute ./configure --prefix="$workspace" --disable-ladspa
-        execute make "-j$threads" --prefix="$workspace" install-static
+        execute make "-j$threads" PREFIX="$workspace" install-static
         build_done "$repo_name" "$version"
     fi
     CONFIGURE_OPTIONS+=("--enable-librubberband")
